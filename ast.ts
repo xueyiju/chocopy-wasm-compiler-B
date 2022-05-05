c4e8e38804c9081e2208bd3e2d5763da945f89c5
Subha Ramesh <17068535+techno96@users.noreply.github.com>
2022-05-05 15:01:04 -0700

    Test change

--- ast.ts
+++ ast.ts
@@ -28,7 +28,7 @@ export type Stmt<A> =
   | {  a?: A, tag: "while", cond: Expr<A>, body: Array<Stmt<A>> }
 
 export type Expr<A> =
-    {  a?: A, tag: "literal", value: Literal }
+  | {  a?: A, tag: "literal", value: Literal }
   | {  a?: A, tag: "id", name: string }
   | {  a?: A, tag: "binop", op: BinOp, left: Expr<A>, right: Expr<A>}
   | {  a?: A, tag: "uniop", op: UniOp, expr: Expr<A> }

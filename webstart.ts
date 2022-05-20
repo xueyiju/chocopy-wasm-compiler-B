f43cb3ab5645f601c002ba534cac2c4801493a4e
Ningzi Zhan <qannap@126.com>
2022-05-20 11:30:22 -0700

    add drag bar

--- webstart.ts
+++ webstart.ts
@@ -1,12 +1,10 @@
-import { BasicREPL } from './repl';
+import { BasicREPL} from './repl';
 import { Type, Value } from './ast';
 import { defaultTypeEnv } from './type-check';
 import { NUM, BOOL, NONE } from './utils';
 // import CodeMirror from "codemirror";
 import { renderResult, renderError, renderPrint } from "./ouputrender";
 
-
-console.log('this is normoal')
 import CodeMirror from "codemirror";
 import "codemirror/addon/edit/closebrackets";
 import "codemirror/mode/python/python";
@@ -16,6 +14,7 @@ import "codemirror/addon/fold/foldgutter";
 import "codemirror/addon/fold/brace-fold";
 import "codemirror/addon/fold/comment-fold";
 import "./style.scss";
+import { log } from 'console';
 
 function assert_not_none(arg: any) : any {
   if (arg === 0)
@@ -23,12 +22,10 @@ function assert_not_none(arg: any) : any {
   return arg;
 }
 
-console.log('this is ok')
 function webStart() {
   var filecontent: string | ArrayBuffer;
   document.addEventListener("DOMContentLoaded", async function() {
 
-    console.log('this is okk')
     // https://github.com/mdn/webassembly-examples/issues/5
 
     const memory = new WebAssembly.Memory({ initial: 10, maximum: 100 });
@@ -55,37 +52,7 @@ function webStart() {
     };
     var repl = new BasicREPL(importObject);
 
-    function setupRepl() {
-      document.getElementById("output").innerHTML = "";
-      const replCodeElement = document.getElementById("next-code") as HTMLTextAreaElement;
-      replCodeElement.addEventListener("keypress", (e) => {
-
-        if (e.shiftKey && e.key === "Enter") {
-        } else if (e.key === "Enter") {
-          e.preventDefault();
-          const output = document.createElement("div");
-          const prompt = document.createElement("span");
-          prompt.innerText = "»";
-          output.appendChild(prompt);
-          const elt = document.createElement("textarea");
-          // elt.type = "text";
-          elt.disabled = true;
-          elt.className = "repl-code";
-          output.appendChild(elt);
-          document.getElementById("output").appendChild(output);
-          const source = replCodeElement.value;
-          elt.value = source;
-          replCodeElement.value = "";
-          repl.run(source).then((r) => {
-            console.log(r);
-            var objectTrackList = repl.trackObject(r, repl.trackHeap());
-            renderResult(r, objectTrackList);
-            console.log("run finished");
-          })
-            .catch((e) => { renderError(e); console.log("run failed", e) });;
-        }
-      });
-    }
+    setupRepl(repl);
 
     function resetRepl() {
       document.getElementById("output").innerHTML = "";
@@ -125,7 +92,7 @@ function webStart() {
 
     document.getElementById("import").addEventListener("click", function () {
       document.getElementById("choose_file").click();
-    })
+    });
 
     document.getElementById("save").addEventListener("click", function (e) {
       //download the code in the user-code text area
@@ -135,8 +102,6 @@ function webStart() {
       var blob = new Blob([source], { type: "text/plain;charset=utf-8" });
       FileSaver.saveAs(blob, title);
     });
-
-    setupRepl();
 
     const textarea = document.getElementById("user-code") as HTMLTextAreaElement;
     const editor = CodeMirror.fromTextArea(textarea, {
@@ -166,7 +131,108 @@ function webStart() {
         (editor as any).showHint({
         });
     });
+
+    dragbarFunction();
+    promptTextArea();
+
+  });
+
+}
+
+function dragbarFunction(){
+  var bar = document.getElementById("dragbar") as HTMLElement;
+  console.log(bar);
+  var wrapper = bar.closest('.dynamic-content-border') as HTMLElement;
+  var codeEditor = wrapper.querySelector('.absolute-content-border') as HTMLElement;
+  var isDragging = false;
+
+  document.addEventListener('mousedown', function(e){
+    if(e.target === bar){
+      isDragging = true;
+    }
+  });
+
+  document.addEventListener('mousemove', function(e){
+    if(!isDragging){
+      return false;
+    }
+
+    var containerOffsetLeft = wrapper.offsetLeft;
+    var pointerRelativeXpos = e.clientX - containerOffsetLeft;
+    var editorMinWidth = 60;
+
+    codeEditor.style.width = (Math.max(editorMinWidth, pointerRelativeXpos - 8)) + 'px';
+    codeEditor.style.flexGrow = '0';
+
+  });
+
+  document.addEventListener('mouseup', function(e){
+    isDragging = false;
   });
 }
 
+function setupRepl(repl: BasicREPL) {
+  document.getElementById("output").innerHTML = "";
+  const replCodeElement = document.getElementById("next-code") as HTMLTextAreaElement;
+  replCodeElement.addEventListener("keydown", (e)=>{
+    console.log(e);
+  })
+  replCodeElement.addEventListener("keypress", (e) => {
+
+    console.log(e);
+    console.log(replCodeElement.selectionStart);
+    console.log(replCodeElement.selectionEnd);
+
+    if (e.shiftKey && e.key === "Enter") {
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+      const source = replCodeElement.value;
+
+      const output = document.createElement("div");
+      const prompt = document.createElement("span");
+      prompt.innerText = "» " + source;
+      output.appendChild(prompt);
+      // const elt = document.createElement("textarea");
+      // // elt.type = "text";
+      // elt.disabled = true;
+      // elt.className = "repl-code";
+      // output.appendChild(elt);
+      document.getElementById("output").appendChild(output);
+      
+      // elt.value = source;
+      replCodeElement.value = "";
+
+      if(source === ""){
+        return false;
+      }
+      repl.run(source).then((r) => {
+        console.log(r);
+        var objectTrackList = repl.trackObject(r, repl.trackHeap());
+        renderResult(r, objectTrackList);
+        console.log("run finished");
+      })
+        .catch((e) => { renderError(e); console.log("run failed", e) });;
+    }
+  });
+}
+
+function promptTextArea(){
+  document.getElementById("interactions").addEventListener("click", (e)=>{
+    document.getElementById("next-code").focus();
+  });
+
+  document.getElementById("next-code").addEventListener("focus", (e)=>{
+    document.addEventListener("keypress", (e)=>{
+      console.log(e);
+      var nextCode = document.getElementById("next-code") as HTMLTextAreaElement;
+      nextCode.selectionStart
+    })
+  });
+
+
+
+  
+}
+
 webStart();
+

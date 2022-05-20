16c54dfa0b545a90f4c37e214b279de5ef264e54
1Divija1 <ddevarla@ucsd.edu>
2022-05-19 18:30:20 -0700

    debugstart revert

--- debugstart.ts
+++ debugstart.ts
@@ -6,11 +6,12 @@ import { importObject, addLibs  } from "./tests/import-object.test";
 // entry point for debugging
 async function debug() {
   var source = `
-a : int = 5
-b : int = 10
-a, *b,c,*_ = 3,4,5,6
-
-`
+class C(object):
+  def f(self: C) -> int:
+    if True:
+      return 0
+    else:
+      return`
   const ast = parse(source);
   
   const repl = new BasicREPL(await addLibs());

03e306081df98a1a1e7a1f61010a540a27ab2946
Subha Ramesh <17068535+techno96@users.noreply.github.com>
2022-05-23 08:36:01 -0700

    Resolving a TA comment

--- type-check.ts
+++ type-check.ts
@@ -258,6 +258,8 @@ export function tcStmt(env : GlobalTypeEnv, locals : LocalTypeEnv, stmt : Stmt<S
 
 export function tcDestructure(env : GlobalTypeEnv, locals : LocalTypeEnv, destr : DestructureLHS<SourceLocation>) : DestructureLHS<[Type, SourceLocation]> {
   
+  // If it is an Ignore variable, do an early return as we don't need
+  // to type-check
   if (destr.lhs.tag === "id" && destr.lhs.name === "_") {
     return {...destr, a:[NONE, destr.a], lhs : {...destr.lhs, a: [NONE, destr.lhs.a]}}
   }
